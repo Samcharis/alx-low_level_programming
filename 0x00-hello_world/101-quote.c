5377020544c00f248ef5e0cee2f24d794f0389bf
21flare <me@gmail.com>
2023-02-20 10:13:48 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success)
+ * main- Entry point
+ *
+ * Description: A program that prints to standard error
+ *
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Kor par, 2015-10-19\n", stdout);
-return (1);
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
