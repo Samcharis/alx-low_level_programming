c9f0a470754529fd4254e494d47274e2c25ce74b
21flare <me@gmail.com>
2023-03-02 10:56:37 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,8 +2,8 @@
 
 /**
  * cap_string - Capitalizes all words of a string
- * @str: the string to be capitalized
- * Return: a pointer to the changed string
+ * @str: The string to be capitalized
+ * Return: A pointer to the changed string
  */
 char *cap_string(char *str)
 {
