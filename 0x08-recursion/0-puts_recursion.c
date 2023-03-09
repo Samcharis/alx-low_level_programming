b82509742074beceb7dc88bd5fa643965e048c1c
21flare <me@gmail.com>
2023-03-09 07:46:21 -0800

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recurstions - prints a string, followed by a new line
+ * @s: the string is printed
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursions(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
