52789bd3dca2f6ac7d1628ea5945dcee20ddc481
samcharis <samcharismouiche21@gmail.com>
2023-10-09 15:09:32 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+/**
+ * main - code space
+ *
+ * return - always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n" sizeof(long int));
+	printf("Size of a long lomng int: %lu byte(s)\n" sizeof(long long int));
+	printf("Size of a long float: %lu byte(s)\n", sizeof(float));
+
+	return (0);
+}
