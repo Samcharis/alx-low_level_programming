ddecc90e159fb74d68548eee9871715ea58d9fe2
samcharis <samcharismouiche21@gmail.com>
2023-10-09 12:30:00 -0700

    2-assembler

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return - Always 0
+ */
 
 int main(void)
 {
@@ -8,13 +13,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n < 0) {
-		printf("%d is negetive \n ", n);
-	} else if (n > 0) {
-		printf("%d is positive \n ", n);
-	} else {
-	       printf("%d is zero \n ", n);
+
+	if (n < 0)
+	{
+		printf("%dis negetive \n", n);
+	} else if (n > 0)
+	{
+		printf("%dis positive \n", n);
+	} else
+	{
+	       printf("%dis zero \n", n);
 	}
-return 0;
+return (0);
 }
