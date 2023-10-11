b2f77aef190dc9821aada66c68c2623f0ef29938
samcharis <samcharismouiche21@gmail.com>
2023-10-11 09:34:15 -0700

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		_putchar("%c ", c);
+
+	return (0);
+}
