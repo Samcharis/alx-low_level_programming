050c361761e869c18b2349d2d92af4df7898f748
samcharis <samcharismouiche21@gmail.com>
2023-10-11 16:08:21 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#inlcude "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (successs)
+ */
+
+void print_alphabet_x10(void);
+{
+	int a;
+
+	char b;
+
+	for (a = 0; i < 10; ++a)
+	{
+		for (b = 'a'; c <= 'z'; ++b)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+
+	return (0);
+}
