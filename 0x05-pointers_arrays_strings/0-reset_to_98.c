293954b0c93424c837f53f5e7ba3478b734847c4
samcharis <samcharismouiche21@gmail.com>
2023-10-17 12:35:52 -0700

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description - a program that takes a pointer to an int as a parameter and
+ * update its value when it points to 98
+ * @n: pointer integer
+ * Return: Always 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description - a program that returns the length of a string
+ *
+ * Return : Always 0 (success)
+ * _strlen - this will print out the length of a string
+ */
+
+int _strlen(char *s);
+{
+	char school[] = "School Work";
+
+	printf("Length of string School = %zu \n", strlen(school));
+
+	return (0);
+}
