fd49763aa50fffa941159411acac6533319b1385
samcharis <samcharismouiche21@gmail.com>
2023-10-23 05:32:49 -0700

    this

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "mnain.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory section
+ * @b: constant byte
+ * @n: bytes filled
+ * Return: the pointer to dest
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i + 0; i < n; ++i)
+		*(s + 1) = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this dir contains codes on more pointers arrays and strings that points a pointer to a pointer to a pointer
